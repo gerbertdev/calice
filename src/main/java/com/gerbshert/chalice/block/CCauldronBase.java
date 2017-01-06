839004d5484477494f262771b9fbebe057948e2a
GerbShert <gabe.sherrett@gmail.com>
2017-01-06 14:12:21 -0800

    Updated Branch to Mod Version 2.6

--- src/main/java/com/gerbshert/chalice/block/CCauldronBase.java
+++ src/main/java/com/gerbshert/chalice/block/CCauldronBase.java
@@ -48,7 +48,7 @@ public class CCauldronBase extends Block {
         this.setDefaultState(this.blockState.getBaseState().withProperty(LEVEL, Integer.valueOf(0)));
     }
 
-    public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn) {
+    public void addCollisionBoxToList(IBlockState state, World world, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, @Nullable Entity entityIn) {
         addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_LEGS);
         addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_WALL_WEST);
         addCollisionBoxToList(pos, entityBox, collidingBoxes, AABB_WALL_NORTH);
@@ -74,43 +74,43 @@ public class CCauldronBase extends Block {
     /**
      * Called When an Entity Collided with the Block
      */
-    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+    public void onEntityCollidedWithBlock(World world, BlockPos pos, IBlockState state, Entity entityIn) {
         int i = ((Integer) state.getValue(LEVEL)).intValue();
         float f = (float) pos.getY() + (6.0F + (float) (3 * i)) / 16.0F;
 
-        if (!worldIn.isRemote && entityIn.isBurning() && i > 0 && entityIn.getEntityBoundingBox().minY <= (double) f) {
+        if (!world.isRemote && entityIn.isBurning() && i > 0 && entityIn.getEntityBoundingBox().minY <= (double) f) {
             entityIn.extinguish();
-            this.setWaterLevel(worldIn, pos, state, i - 1);
+            this.setWaterLevel(world, pos, state, i - 1);
         }
     }
 
-    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing heldItem, float side, float hitX, float hitY) {
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing heldItem, float side, float hitX, float hitY) {
         ItemStack itemstack = playerIn.getHeldItem(hand);
 
-        if (itemstack.func_190926_b()) {
+        if (itemstack.isEmpty()) {
             return true;
         } else {
             int i = ((Integer) state.getValue(LEVEL)).intValue();
             Item item = itemstack.getItem();
 
             if (item == Items.WATER_BUCKET) {
-                if (i < 3 && !worldIn.isRemote) {
+                if (i < 3 && !world.isRemote) {
                     if (!playerIn.capabilities.isCreativeMode) {
                         playerIn.setHeldItem(hand, new ItemStack(Items.BUCKET));
                     }
 
                     playerIn.addStat(StatList.CAULDRON_FILLED);
-                    this.setWaterLevel(worldIn, pos, state, 3);
-                    worldIn.playSound((EntityPlayer) null, pos, SoundEvents.ITEM_BUCKET_EMPTY, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    this.setWaterLevel(world, pos, state, 3);
+                    world.playSound((EntityPlayer) null, pos, SoundEvents.ITEM_BUCKET_EMPTY, SoundCategory.BLOCKS, 1.0F, 1.0F);
                 }
 
                 return true;
             } else if (item == Items.BUCKET) {
-                if (i == 3 && !worldIn.isRemote) {
+                if (i == 3 && !world.isRemote) {
                     if (!playerIn.capabilities.isCreativeMode) {
-                        itemstack.func_190918_g(1);
+                        itemstack.shrink(1);
 
-                        if (itemstack.func_190926_b()) {
+                        if (itemstack.isEmpty()) {
                             playerIn.setHeldItem(hand, new ItemStack(Items.WATER_BUCKET));
                         } else if (!playerIn.inventory.addItemStackToInventory(new ItemStack(Items.WATER_BUCKET))) {
                             playerIn.dropItem(new ItemStack(Items.WATER_BUCKET), false);
@@ -118,19 +118,19 @@ public class CCauldronBase extends Block {
                     }
 
                     playerIn.addStat(StatList.CAULDRON_USED);
-                    this.setWaterLevel(worldIn, pos, state, 0);
-                    worldIn.playSound((EntityPlayer) null, pos, SoundEvents.ITEM_BUCKET_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    this.setWaterLevel(world, pos, state, 0);
+                    world.playSound((EntityPlayer) null, pos, SoundEvents.ITEM_BUCKET_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
                 }
 
                 return true;
             } else if (item == Items.GLASS_BOTTLE) {
-                if (i > 0 && !worldIn.isRemote) {
+                if (i > 0 && !world.isRemote) {
                     if (!playerIn.capabilities.isCreativeMode) {
                         ItemStack itemstack3 = PotionUtils.addPotionToItemStack(new ItemStack(Items.POTIONITEM), PotionTypes.WATER);
                         playerIn.addStat(StatList.CAULDRON_USED);
-                        itemstack.func_190918_g(1);
+                        itemstack.shrink(1);
 
-                        if (itemstack.func_190926_b()) {
+                        if (itemstack.isEmpty()) {
                             playerIn.setHeldItem(hand, itemstack3);
                         } else if (!playerIn.inventory.addItemStackToInventory(itemstack3)) {
                             playerIn.dropItem(itemstack3, false);
@@ -139,13 +139,13 @@ public class CCauldronBase extends Block {
                         }
                     }
 
-                    worldIn.playSound((EntityPlayer) null, pos, SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
-                    this.setWaterLevel(worldIn, pos, state, i - 1);
+                    world.playSound((EntityPlayer) null, pos, SoundEvents.ITEM_BOTTLE_FILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    this.setWaterLevel(world, pos, state, i - 1);
                 }
 
                 return true;
             } else if (item == Items.POTIONITEM && PotionUtils.getPotionFromItem(itemstack) == PotionTypes.WATER) {
-                if (i < 3 && !worldIn.isRemote) {
+                if (i < 3 && !world.isRemote) {
                     if (!playerIn.capabilities.isCreativeMode) {
                         ItemStack itemstack2 = new ItemStack(Items.GLASS_BOTTLE);
                         playerIn.addStat(StatList.CAULDRON_USED);
@@ -156,8 +156,8 @@ public class CCauldronBase extends Block {
                         }
                     }
 
-                    worldIn.playSound((EntityPlayer) null, pos, SoundEvents.field_191241_J, SoundCategory.BLOCKS, 1.0F, 1.0F);
-                    this.setWaterLevel(worldIn, pos, state, i + 1);
+                    world.playSound((EntityPlayer) null, pos, SoundEvents.ITEM_BOTTLE_EMPTY, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                    this.setWaterLevel(world, pos, state, i + 1);
                 }
 
                 return true;
@@ -165,27 +165,27 @@ public class CCauldronBase extends Block {
                 if (i > 0 && item instanceof ItemArmor) {
                     ItemArmor itemarmor = (ItemArmor) item;
 
-                    if (itemarmor.getArmorMaterial() == ItemArmor.ArmorMaterial.LEATHER && itemarmor.hasColor(itemstack) && !worldIn.isRemote) {
+                    if (itemarmor.getArmorMaterial() == ItemArmor.ArmorMaterial.LEATHER && itemarmor.hasColor(itemstack) && !world.isRemote) {
                         itemarmor.removeColor(itemstack);
-                        this.setWaterLevel(worldIn, pos, state, i - 1);
+                        this.setWaterLevel(world, pos, state, i - 1);
                         playerIn.addStat(StatList.ARMOR_CLEANED);
                         return true;
                     }
                 }
 
                 if (i > 0 && item instanceof ItemBanner) {
-                    if (TileEntityBanner.getPatterns(itemstack) > 0 && !worldIn.isRemote) {
+                    if (TileEntityBanner.getPatterns(itemstack) > 0 && !world.isRemote) {
                         ItemStack itemstack1 = itemstack.copy();
-                        itemstack1.func_190920_e(1);
+                        itemstack1.setCount(1);
                         TileEntityBanner.removeBannerData(itemstack1);
                         playerIn.addStat(StatList.BANNER_CLEANED);
 
                         if (!playerIn.capabilities.isCreativeMode) {
-                            itemstack.func_190918_g(1);
-                            this.setWaterLevel(worldIn, pos, state, i - 1);
+                            itemstack.shrink(1);
+                            this.setWaterLevel(world, pos, state, i - 1);
                         }
 
-                        if (itemstack.func_190926_b()) {
+                        if (itemstack.isEmpty()) {
                             playerIn.setHeldItem(hand, itemstack1);
                         } else if (!playerIn.inventory.addItemStackToInventory(itemstack1)) {
                             playerIn.dropItem(itemstack1, false);
@@ -202,23 +202,23 @@ public class CCauldronBase extends Block {
         }
     }
 
-    public void setWaterLevel(World worldIn, BlockPos pos, IBlockState state, int level) {
-        worldIn.setBlockState(pos, state.withProperty(LEVEL, Integer.valueOf(MathHelper.clamp_int(level, 0, 3))), 2);
-        worldIn.updateComparatorOutputLevel(pos, this);
+    public void setWaterLevel(World world, BlockPos pos, IBlockState state, int level) {
+        world.setBlockState(pos, state.withProperty(LEVEL, Integer.valueOf(MathHelper.clamp(level, 0, 3))), 2);
+        world.updateComparatorOutputLevel(pos, this);
     }
 
     /**
      * Called similar to random ticks, but only when it is raining.
      */
-    public void fillWithRain(World worldIn, BlockPos pos) {
-        if (worldIn.rand.nextInt(20) == 1) {
-            float f = worldIn.getBiome(pos).getFloatTemperature(pos);
+    public void fillWithRain(World world, BlockPos pos) {
+        if (world.rand.nextInt(20) == 1) {
+            float f = world.getBiome(pos).getFloatTemperature(pos);
 
-            if (worldIn.getBiomeProvider().getTemperatureAtHeight(f, pos.getY()) >= 0.15F) {
-                IBlockState iblockstate = worldIn.getBlockState(pos);
+            if (world.getBiomeProvider().getTemperatureAtHeight(f, pos.getY()) >= 0.15F) {
+                IBlockState iblockstate = world.getBlockState(pos);
 
                 if (((Integer) iblockstate.getValue(LEVEL)).intValue() < 3) {
-                    worldIn.setBlockState(pos, iblockstate.cycleProperty(LEVEL), 2);
+                    world.setBlockState(pos, iblockstate.cycleProperty(LEVEL), 2);
                 }
             }
         }
@@ -242,7 +242,7 @@ public class CCauldronBase extends Block {
         return new BlockStateContainer(this, new IProperty[]{LEVEL});
     }
 
-    public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
+    public boolean isPassable(IBlockAccess world, BlockPos pos) {
         return true;
     }
 }
--- src/main/java/com/gerbshert/chalice/libraries/Strings.java
+++ src/main/java/com/gerbshert/chalice/libraries/Strings.java
@@ -6,5 +6,5 @@ package com.gerbshert.chalice.libraries;
 public class Strings {
     public static final String MOD_NAME = "Chalice";
     public static final String MOD_ID = "chalice";
-    public static final String MOD_VERSION = "1.11-2.6";
+    public static final String MOD_VERSION = "1.11.2-2.6";
 }
